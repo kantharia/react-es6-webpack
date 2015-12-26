e4de7e91893b1fe318016c66a178a5fe487b9eae
Chetan Kantharia <chetan.kantharia@gmail.com>
2015-12-26 19:31:32 +0530

    React Props: - Learnt about passing props to component - PropTypes and Default Props

--- App.js
+++ App.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    let txt = this.props.txt;
+    return <h1>{txt}</h1>;
   }
 }
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+  txt: 'This is the default props'
+};
 
 export default App;
