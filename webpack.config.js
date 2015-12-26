1f4f9a7f24238177293eef00b0168df127400d00
Chetan Kantharia <chetan.kantharia@gmail.com>
2015-12-26 13:37:53 +0530

    Initial Commit: - Install react, react-dom, babel and webpack for dev enviornment

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry:'./main.js',
+  output:{
+    path:'./',
+    filename:'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
