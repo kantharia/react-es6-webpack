abd9bfaa43bf991e2c322ee166d2c6a245240652
Chetan Kantharia <chetan.kantharia@gmail.com>
2015-12-29 15:28:41 +0530

    React State: - Understaning and updateing state via. setState()

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { txt: 'This is the state.'};
+  }
+
+  update(e) {
+    this.setState({
+      txt:e.target.value
+    });
+  }
+
   render() {
-    let txt = this.props.txt;
-    return <h1>{txt}</h1>;
+    let txt = this.state.txt;
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{txt}</h1>
+      </div>
+    );
   }
 }
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-};
-
-App.defaultProps = {
-  txt: 'This is the default props'
-};
 
 export default App;
